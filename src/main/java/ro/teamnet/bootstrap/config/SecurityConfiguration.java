2aeea2c44c49c6346206c0c67b8d11e932340779
Oana.Mihai <oana.mihai@teamnet.ro>
2015-09-30 15:40:37 +0300

    added support for pre-authentication filters

--- src/main/java/ro/teamnet/bootstrap/config/SecurityConfiguration.java
+++ src/main/java/ro/teamnet/bootstrap/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.plugin.core.PluginRegistry;
 import org.springframework.plugin.core.config.EnablePluginRegistries;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
@@ -21,17 +22,21 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 import ro.teamnet.bootstrap.constants.AuthoritiesConstants;
 import ro.teamnet.bootstrap.plugin.security.AuthenticationProviderPlugin;
+import ro.teamnet.bootstrap.plugin.security.PreAuthenticationFilterPlugin;
+import ro.teamnet.bootstrap.plugin.security.PreAuthenticationFilterType;
 import ro.teamnet.bootstrap.plugin.security.UserDetailsDecoratorPlugin;
 import ro.teamnet.bootstrap.security.*;
 
 import javax.inject.Inject;
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
 @EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
-@EnablePluginRegistries({AuthenticationProviderPlugin.class, UserDetailsDecoratorPlugin.class})
+@EnablePluginRegistries({AuthenticationProviderPlugin.class, UserDetailsDecoratorPlugin.class, PreAuthenticationFilterPlugin.class})
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final Logger log = LoggerFactory.getLogger(SecurityConfiguration.class);
@@ -62,6 +67,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Inject
     @Qualifier(value = "customAuthenticationProviderService")
     private CustomAuthenticationProviderService customAuthenticationProviderService;
+
+    @Inject
+    @Qualifier("preAuthenticationFilterPluginRegistry")
+    private PluginRegistry<PreAuthenticationFilterPlugin, PreAuthenticationFilterType> preAuthenticationFilterPluginRegistry;
 
     @Inject
     private RememberMeServices rememberMeServices;
@@ -100,14 +109,74 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .exceptionHandling()
-                .authenticationEntryPoint(authenticationEntryPoint)
-                .and()
-                .rememberMe()
-                .rememberMeServices(rememberMeServices)
-                .key(env.getProperty("jhipster.security.rememberme.key"))
-                .and()
+        boolean shouldSkipLogin = shouldSkipLogin();
+        HttpSecurity httpSecurity = shouldSkipLogin
+                ? http.exceptionHandling().and()
+                : http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint).and();
+        httpSecurity = addRememberMe(httpSecurity);
+        for (PreAuthenticationFilterPlugin preAuthenticationFilterPlugin : preAuthenticationFilterPluginRegistry.getPlugins()) {
+            Filter preAuthenticationFilter = preAuthenticationFilterPlugin.getFilter();
+            if (preAuthenticationFilterPlugin.supports(PreAuthenticationFilterType.PROVIDES_AUTHENTICATION)
+                    && preAuthenticationFilter instanceof AbstractPreAuthenticatedProcessingFilter) {
+                ((AbstractPreAuthenticatedProcessingFilter)preAuthenticationFilter).setAuthenticationManager(authenticationManager());
+            }
+            httpSecurity = httpSecurity.addFilter(preAuthenticationFilter);
+        }
+        if (!shouldSkipLogin) {
+            httpSecurity = addLogin(httpSecurity);
+            addLogout(httpSecurity)
+                    .csrf()
+                    .disable()
+                    .headers()
+                    .frameOptions()
+                    .disable()
+                    .authorizeRequests()
+                    .antMatchers("/app/cfg/app.json").permitAll()
+                    .antMatchers("/app/rest/publicAccount/register").permitAll()
+                    .antMatchers("/app/rest/activateAccount/activate").permitAll()
+                    .antMatchers("/app/rest/dictionaryElement/**").permitAll()
+                    .antMatchers("/app/rest/authenticate").permitAll()
+                    .antMatchers("/app/rest/role").authenticated()
+                    .antMatchers("/app/rest/logs/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                    .antMatchers("/app/**").authenticated()
+                    .antMatchers("/websocket/tracker").hasAuthority(AuthoritiesConstants.ADMIN)
+                    .antMatchers("/websocket/**").permitAll()
+                    .antMatchers("/metrics/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                    .antMatchers("/health/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                    .antMatchers("/trace/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                    .antMatchers("/dump/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                    .antMatchers("/shutdown/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                    .antMatchers("/beans/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                    .antMatchers("/configprops/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                    .antMatchers("/info/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                    .antMatchers("/autoconfig/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                    .antMatchers("/env/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                    .antMatchers("/trace/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                    .antMatchers("/api-docs/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                    .antMatchers("/protected/**").authenticated();
+        }
+        else {
+            addLogout(httpSecurity)
+                    .csrf()
+                    .disable()
+                    .headers()
+                    .frameOptions()
+                    .disable()
+                    .authorizeRequests()
+                    .antMatchers("/app/cfg/app.json").permitAll()
+                    .antMatchers("/app/rest/publicAccount/register").permitAll()
+                    .antMatchers("/app/rest/activateAccount/activate").permitAll()
+                    .antMatchers("/app/rest/dictionaryElement/**").permitAll()
+                    .antMatchers("/app/rest/authenticate").permitAll()
+                    .antMatchers("/app/rest/role").authenticated()
+                    .antMatchers("/app/**").authenticated()
+                    .antMatchers("/websocket/**").permitAll()
+                    .antMatchers("/protected/**").authenticated();
+        }
+    }
+
+    private HttpSecurity addLogin(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
                 .formLogin()
                 .loginProcessingUrl("/app/authentication")
                 .successHandler(ajaxAuthenticationSuccessHandler)
@@ -115,43 +184,30 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .usernameParameter("j_username")
                 .passwordParameter("j_password")
                 .permitAll()
-                .and()
+                .and();
+    }
+
+    private HttpSecurity addLogout(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
                 .logout()
                 .logoutUrl("/app/logout")
                 .logoutSuccessHandler(ajaxLogoutSuccessHandler)
                 .deleteCookies("JSESSIONID")
                 .permitAll()
-                .and()
-                .csrf()
-                .disable()
-                .headers()
-                .frameOptions()
-                .disable()
-                .authorizeRequests()
-                .antMatchers("/app/cfg/app.json").permitAll()
-                .antMatchers("/app/rest/publicAccount/register").permitAll()
-                .antMatchers("/app/rest/activateAccount/activate").permitAll()
-                .antMatchers("/app/rest/dictionaryElement/**").permitAll()
-                .antMatchers("/app/rest/authenticate").permitAll()
-                .antMatchers("/app/rest/role").authenticated()
-                .antMatchers("/app/rest/logs/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                .antMatchers("/app/**").authenticated()
-                .antMatchers("/websocket/tracker").hasAuthority(AuthoritiesConstants.ADMIN)
-                .antMatchers("/websocket/**").permitAll()
-                .antMatchers("/metrics/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                .antMatchers("/health/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                .antMatchers("/trace/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                .antMatchers("/dump/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                .antMatchers("/shutdown/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                .antMatchers("/beans/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                .antMatchers("/configprops/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                .antMatchers("/info/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                .antMatchers("/autoconfig/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                .antMatchers("/env/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                .antMatchers("/trace/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                .antMatchers("/api-docs/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                .antMatchers("/protected/**").authenticated();
+                .and();
+    }
 
+    private HttpSecurity addRememberMe(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity = httpSecurity
+                .rememberMe()
+                .rememberMeServices(rememberMeServices)
+                .key(env.getProperty("jhipster.security.rememberme.key"))
+                .and();
+        return httpSecurity;
+    }
+
+    private boolean shouldSkipLogin() {
+        return preAuthenticationFilterPluginRegistry.getPluginFor(PreAuthenticationFilterType.PROVIDES_AUTHENTICATION) != null;
     }
 
     @EnableGlobalMethodSecurity(prePostEnabled = true, jsr250Enabled = true)
